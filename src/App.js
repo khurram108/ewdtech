c04534abbdda5bf885b4ae6a325a283d1f8b9679
TBDev2 <tb_dev2@thunderbees.com>
2020-09-01 16:26:02 +0500

    'as'

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import {   BrowserRouter as Router ,Route, Switch } from 'react-router-dom'
 // import {  Router , Route ,Switch} from 'react-router'
-import HomePage from './components/app'
-import history from './components/common/history'
-import AboutUs from './components/AboutUs/aboutUs'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import ContactUs from './components/contactUs/contact'
-import OurTeam from './components/ourTeam/ourTeam'
-import Portfolio from './portfolio/portfolio'
-import {
-  Router,
-  Switch,
-  Route,
-  Link,
+import HomePage from "./components/app";
+import history from "./components/common/history";
+import AboutUs from "./components/AboutUs/aboutUs.js";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ContactUs from "./components/contactUs/contact";
+import OurTeam from "./components/ourTeam/ourTeam";
+import Portfolio from "./portfolio/portfolio";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-} from "react-router-dom";
-
-function App() {
-
-
-
+const App = () => {
   return (
-
-
-    <div>
-      <Header />
-      <Portfolio />
-
-
-      {/* <Router history={history}>
-        <Switch>
-
-          <Route path="/" component={HomePage} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/about_us" component={AboutUs} />
-
-        </Switch>
-
-      </Router> */}
-    </div>
-
-
-
-  )
-}
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutUs} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,112 +1,106 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { Link, BrowserRouter, withRouter } from 'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Link, BrowserRouter as Router, withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        overflowX: 'hidden',
-        // position: 'absolute',
-        position: 'sticky',
-        top: 0,
-        backgroundColor: 'white',
-        boxShadow: "0px 8px 6px -6px lightgrey",
-        zIndex: 999,
-        height: 100,
-    },
-    logo: {
-        padding: 15,
-        paddingLeft: 50
-    },
-    navOuter: {
-        padding: 10,
-        paddingLeft: 50,
-        display: "flex",
-        height: 80,
-        position: 'relative',
-        justifyContent: 'flex-end',
-        right: 40
-
-    },
-    tab: {
-        width: "100px !important"
-    },
-
-
-
+  root: {
+    flexGrow: 1,
+    overflowX: "hidden",
+    // position: 'absolute',
+    position: "sticky",
+    top: 0,
+    backgroundColor: "white",
+    boxShadow: "0px 8px 6px -6px lightgrey",
+    zIndex: 999,
+    height: 100,
+  },
+  logo: {
+    padding: 15,
+    paddingLeft: 50,
+  },
+  navOuter: {
+    padding: 10,
+    paddingLeft: 50,
+    display: "flex",
+    height: 80,
+    position: "relative",
+    justifyContent: "flex-end",
+    right: 40,
+  },
+  tab: {
+    width: "100px !important",
+  },
 }));
 
 function Header() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [active, setActive] = React.useState(0);
+  const [active, setActive] = React.useState(0);
 
-    const handleChange = (index) => {
-        setActive(index);
-    };
+  const handleChange = (index) => {
+    setActive(index);
+  };
 
-    return (
-        <>
-
-            <BrowserRouter>
-                <div className="container-fluid header-fluid">
-
-                    <Grid container className={classes.root} >
-                        <Grid item xs={12} md={4} spacing={2}>
-                            <Grid justify="space-around">
-                                <Link to='/home'>
-                                    <img src={require('../resources/Home/images/logo-8.png')} alt="" className={classes.logo} />
-                                </Link>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} md={8} className={classes.navOuter}>
-                            <div className={classes.navOuter}>
-                                <li className={`nav-item ${active == 0 ? "active-tab" : null}`} onClick={() => handleChange(0)}>
-                                    <Link to='/home'>
-                                        <span className="nav-text">
-
-                                            Home
-                </span>
-                                    </Link>
-                                </li>
-                                <li className={`nav-item ${active == 1 ? "active-tab" : null}`} onClick={() => handleChange(1)}>
-                                    <span className="nav-text">
-                                        Services
-            </span>
-                                </li>
-                                <li className={`nav-item ${active == 2 ? "active-tab" : null}`} onClick={() => handleChange(2)}>
-                                    <span className="nav-text">
-                                        Portfolio
-            </span>
-                                </li>
-                                <Link to="/about_us">
-                                    <li className={`nav-item ${active == 3 ? "active-tab" : null}`} onClick={() => handleChange(3)}>
-                                        <span className="nav-text">
-                                             About Us
-                                        </span>
-                                    </li>
-                                </Link>
-                                <li className={`nav-item ${active == 4 ? "active-tab" : null}`} onClick={() => handleChange(4)}>
-                                    <span className="nav-text">
-                                        Contact Us
-            </span>
-                                </li>
-                                {/* <img src={require('../resources/Home/images/chair.png')} alt=""/> */}
-                            </div>
-
-                        </Grid>
-                    </Grid>
-
-                </div>
-            </BrowserRouter>
-
-        </>
-
-    )
-
-
+  return (
+    <div>
+ 
+        <div className="container-fluid header-fluid">
+          <Grid container className={classes.root}>
+            <Grid item xs={12} md={4} spacing={2}>
+              <Grid justify="space-around">
+                <Link to="/home">
+                  <img
+                    src={require("../resources/Home/images/logo-8.png")}
+                    alt=""
+                    className={classes.logo}
+                  />
+                </Link>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={8} className={classes.navOuter}>
+              <div className={classes.navOuter}>
+                <li
+                  className={`nav-item ${active == 0 ? "active-tab" : null}`}
+                  onClick={() => handleChange(0)}
+                >
+                  <Link to="/home">
+                    <span className="nav-text">Home</span>
+                  </Link>
+                </li>
+                <li
+                  className={`nav-item ${active == 1 ? "active-tab" : null}`}
+                  onClick={() => handleChange(1)}
+                >
+                  <span className="nav-text">Services</span>
+                </li>
+                <li
+                  className={`nav-item ${active == 2 ? "active-tab" : null}`}
+                  onClick={() => handleChange(2)}
+                >
+                  <span className="nav-text">Portfolio</span>
+                </li>
+                <Link to="/about">
+                  <li
+                    className={`nav-item ${active == 3 ? "active-tab" : null}`}
+                    onClick={() => handleChange(3)}
+                  >
+                    <span className="nav-text">About Us</span>
+                  </li>
+                </Link>
+                <li
+                  className={`nav-item ${active == 4 ? "active-tab" : null}`}
+                  onClick={() => handleChange(4)}
+                >
+                  <span className="nav-text">Contact Us</span>
+                </li>
+                {/* <img src={require('../resources/Home/images/chair.png')} alt=""/> */}
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+    </div>
+  );
 }
 
 export default Header;
